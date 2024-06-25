264839431d43eb63a8adac1d0003ce73bcaa1966
Pedro Lucas Resende Neto <92889871+PedroResendeN@users.noreply.github.com>
2024-06-25 18:35:05 -0300

    estrutura html e css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { FiSearch } from 'react-icons/fi'
+import './Styles.css'
 
 function App () {
   return (
-    <>
-      <div>
-        
-      </div>
-    </>
+    <div className='container'>
+        <h1 className="title">Buscador de CEP</h1>
+        <div className="containerInput">
+          <input type="text" placeholder='Digite seu cep...'/>
+          <button className="buttonSearch">
+            <FiSearch size={25} color='#fff'/>
+          </button>
+        </div>
+
+        <main className='main'>
+          <h2></h2>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </main>
+    </div>
   )
 }
 
