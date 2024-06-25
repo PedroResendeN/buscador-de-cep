c62ac734fd42cd6ffd48db92c6d45823621f5edd
Pedro Lucas Resende Neto <92889871+PedroResendeN@users.noreply.github.com>
2024-06-25 19:36:36 -0300

    lógica
    toda a parte funcional do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,54 @@
-import { FiSearch } from 'react-icons/fi'
-import './Styles.css'
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import Api from './services/Api'
+import './Styles.css';
 
 function App () {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    // 01310930/json/
+
+    if (input === '') {
+      alert('Informe um CEP!')
+      return;
+    }
+
+    try {
+      const response = await Api.get(`${input}/json`);
+      setCep(response.data)
+      setInput('')
+    }catch{
+      alert("Erro ao buscar")
+      setInput('')
+    }
+  }
+
   return (
     <div className='container'>
         <h1 className="title">Buscador de CEP</h1>
         <div className="containerInput">
-          <input type="text" placeholder='Digite seu cep...'/>
-          <button className="buttonSearch">
+          <input type="text" value={input} onChange={(event) => setInput(event.target.value)} placeholder='Digite seu cep...'/>
+          <button className="buttonSearch" onClick={handleSearch}>
             <FiSearch size={25} color='#fff'/>
           </button>
         </div>
 
-        <main className='main'>
-          <h2></h2>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </main>
+        {Object.keys(cep).length > 0 && (
+
+          <main className='main'>
+            <h2>{cep.cep}</h2>
+
+            <span>{cep.logradouro}</span>
+            <span>{cep.complemento}</span>
+            <span>{cep.bairro}</span>
+            <span>{cep.localidade} - {cep.uf}</span>
+          </main>
+
+        )};
+        
     </div>
   )
 }
